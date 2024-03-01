2839d2ad9c33aaf94f0cd2374f3dba2d6929c50b
Gabriel Martin <g.martin@htlkrems.at>
2024-03-01 18:52:20 +0100

    Create WPF Project

---
+++ WpfApp1/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿namespace WpfApp1;
+
+public class MainViewModel : ViewModelBase
+{
+    private int age;
+    private string _input;
+
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            age = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Input
+    {
+        get => _input;
+        set { _input = value; OnPropertyChanged("Input"); }
+    }
+
+    public RelayCommand ParseCommand { get; set; }
+    public RelayCommand ResetCommand { get; set; }
+
+    public MainViewModel()
+    {
+        ParseCommand = new RelayCommand(
+        o =>
+        {
+            bool success = int.TryParse(Input, out int result);
+            if (success)
+                Age = result;
+        },
+        o =>
+        {
+            return int.TryParse(Input, out int result);
+        });
+
+        ResetCommand = new RelayCommand(o =>
+        {
+            Age = 0;
+        }, o =>
+        {
+            return Age != 0;
+        });
+    }
+}
---
+++ WpfApp1/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace WpfApp1;
+
+public class RelayCommand : ICommand
+{
+    private Action<object?> execute;
+    private Predicate<object?> canExecute;
+    
+    public RelayCommand(Action<object?> execute, Predicate<object?> canExecute)
+    {
+        this.execute = execute;
+        this.canExecute = canExecute;
+    }
+    
+    public bool CanExecute(object? parameter)
+    {
+        return canExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        execute(parameter);
+    }
+    
+
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+}
